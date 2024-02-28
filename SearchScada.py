f758c132d3c21b14a60c5c46247afb4552636eab
Дмитрий Сергеевич <fllans@mail.ru>
2024-02-28 22:10:59 +0300

    #2 Добавил варианты чтения атрибутов

--- SearchScada.py
+++ SearchScada.py
@@ -1,3 +1,29 @@
 import glob
-qwerty = glob.glob('D:/*/*me*')
-print(qwerty)
+#qwerty = glob.glob('D:/*/*me*')
+#print(qwerty)
+
+
+
+import os
+#import datetime
+#def modification_date(filename):
+#    t = os.path.getmtime(filename)
+#    return datetime.datetime.fromtimestamp(t)
+#print (modification_date("D:/pmsm_ideal.mdl"))
+
+
+
+testpath = input('D:/pmsm_ideal.mdl')
+ 
+
+print(os.path.isfile(testpath))
+
+
+import exifread
+ 
+ 
+def is_lzw(filename):
+    with open(filename, 'rb') as f:
+        return 'LZW' in exifread.process_file(f)['Image Compression'].printable
+ 
+print(is_lzw('D:/pmsm_ideal.mdl'))
